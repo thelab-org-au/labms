a1405b7dbcb8408af08efe7a40decc7211f0a70f
James Cartledge <jcartledge@gmail.com>
2015-07-10 19:28:05 +1000

    [#23] development db config

---
+++ application/config/development/database.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = trim(`boot2docker ip`);
+$db['default']['username'] = 'user';
+$db['default']['password'] = 'password';
+$db['default']['database'] = 'db';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
